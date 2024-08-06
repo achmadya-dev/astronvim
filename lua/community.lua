29ad06c1f246bc69215bdce031ad9749c5babf3a
madyardwn <madyardwn@gmail.com>
2024-08-06 10:38:07 +0700

    madya

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +8,23 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    -- customise the copilot plugin
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          -- accept = "<C-l>", changed to Alt-l
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
